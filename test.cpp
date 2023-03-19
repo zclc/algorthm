f368c8a717045d87781a196c2ae3ea61e4f1d435
zclc <2451433436@qq.com>
2023-03-19 15:01:49 +0800

    update algorithm

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello wrold\n" << endl;
+
+    return 0;
+}
